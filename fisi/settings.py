076f37e2998cb09a491768ece17bdbe2a66bd97d
CodingLinhTinh <92833984+CodingLinhTinh@users.noreply.github.com>
2022-07-11 12:07:16 +0700

    Update settings.py

--- fisi/settings.py
+++ fisi/settings.py
@@ -153,7 +153,7 @@ EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='fisishop.io@gmail.com'
-EMAIL_HOST_PASSWORD='Ngoc2002' 
+EMAIL_HOST_PASSWORD='Ngoc_2002' 
 EMAIL_USE_TLS=True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
